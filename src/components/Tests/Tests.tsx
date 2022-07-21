2266f544145f8a8182ea09e6c865739934d520fe
JohnSloan8 <johnsloan1984@gmail.com>
2022-07-21 14:00:24 +0100

    fixed duplicate key error

--- src/components/Tests/Tests.tsx
+++ src/components/Tests/Tests.tsx
@@ -97,11 +97,8 @@ const Tests = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {testsDisplay.map((test: DisplayTest) => (
-              <TableRow
-                key={test.title + test.language + test.level}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
+            {testsDisplay.map((test: DisplayTest, i: number) => (
+              <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                 <TableCell component="th" scope="row">
                   {test.language}
                 </TableCell>
